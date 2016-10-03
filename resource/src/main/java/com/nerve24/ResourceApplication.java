caec44f0ba6c5dfd56fc9865bf01bfa34fdef9d2
JogiReddy Kotam <jogiwithjosh@gmail.com>
2016-10-03 08:52:12 +0530

    Add files via upload

---
+++ resource/src/main/java/com/nerve24/ResourceApplication.java
@@ -0,0 +1,39 @@
+package com.nerve24;
+
+import org.apache.catalina.filters.RequestDumperFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableResourceServer
+@RestController
+public class ResourceApplication extends ResourceServerConfigurerAdapter{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceApplication.class, args);
+	}
+	
+	@Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/**").access("#oauth2.hasScope('read')")
+                .antMatchers(HttpMethod.POST, "/**").access("#oauth2.hasScope('write')");
+    }
+	
+	@Profile("!cloud")
+    @Bean
+    RequestDumperFilter requestDumperFilter() {
+        return new RequestDumperFilter();
+    }
+}
